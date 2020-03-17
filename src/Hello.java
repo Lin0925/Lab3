ebbd41ed0bf10bc53dea752bc5dc3a36a363c569
林柏民 <@user>
2020-03-17 21:39:53 +0800

    Lab3-HW

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,6 @@
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello everyone,");
-    System.out.println("I am studying at Feng Chia University");
+    System.out.println("I am studying at Feng Chia University.");
   }
 }
